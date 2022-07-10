5a4a66c1ca58d2a8e085bcb900b33d03a107f812
Gamze Gürcan <gmzgrcn18812@icloud.com>
2022-07-10 20:23:08 +0300

    created folder structure, added assets and const

---
+++ lib/core/constant/app_color.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const loginColor = Color(0xFF1317DD);
+}
---
+++ lib/core/constant/app_text.dart
@@ -0,0 +1,15 @@
+class AppText {
+  static const String login = "Login";
+  static const String signUp = "Sign Up";
+  static const String email = "Email";
+  static const String password = "Password";
+  static const String firstName = "First Name";
+  static const String lastName = "Last Name";
+  static const String confirm = "Confirm Password";
+  static const String rememberMe = "Remember Me";
+  static const String forgotPassword = "Forgot Password";
+  static const String account = "Don't have an account?";
+  static const String registernow = "Register Now";
+  static const String loginwith = "Log in with";
+  static const String or = "OR";
+}
